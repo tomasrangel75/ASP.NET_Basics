4da45a2c9a641e4e9ff313798c1cc1220d7a4e8d
Tomas <tomas.rangel75@gmail.com>
2016-05-30 22:23:36 -0300

    20150530 22?23

---
+++ KatanaBasics/KatanaBasics/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(KatanaBasics.ProductionStartup))]
+
+namespace KatanaBasics
+{
+	
+		public class ProductionStartup
+		{
+			public void Configuration(IAppBuilder app)
+			{
+				app.Use((context, next) =>
+				{
+					TextWriter output = context.Get<TextWriter>("host.TraceOutput");
+					return next().ContinueWith(result =>
+					{
+						output.WriteLine("Scheme {0} : Method {1} : Path {2} : MS {3}",
+						context.Request.Scheme, context.Request.Method, context.Request.Path, getTime());
+					});
+				});
+
+				app.Run(async context =>
+				{
+					await context.Response.WriteAsync(getTime() + " My First OWIN App");
+				});
+			}
+
+			string getTime()
+			{
+				return DateTime.Now.Millisecond.ToString();
+			}
+		}
+	
+
+}
